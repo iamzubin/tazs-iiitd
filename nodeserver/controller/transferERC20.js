f6b6b44019bee90b91d010f185af5409622a6940
zubin Choudhary <zchoudhary.10@gmail.com>
2019-08-24 08:01:29 +0530

    node server bindings

---
+++ nodeserver/controller/transferERC20.js
@@ -0,0 +1,30 @@
+const Matic = require('maticjs').default
+const config = require('./config')
+const web3 = require('web3')
+
+const from = config.FROM_ADDRESS // from address
+const recipient = '0x0a545B5408D3ADdD746A09113ceEF9272179769D' // receipent address
+
+const token = config.MATIC_TEST_TOKEN // test token address
+const amount = '100000000000000000' // amount in wei
+
+// Create object of Matic
+const matic = new Matic({
+  maticProvider: config.MATIC_PROVIDER,
+  parentProvider: config.PARENT_PROVIDER,
+  rootChainAddress: config.ROOTCHAIN_ADDRESS,
+  syncerUrl: config.SYNCER_URL,
+  watcherUrl: config.WATCHER_URL,
+})
+
+matic.wallet = config.PRIVATE_KEY // prefix with `0x`
+
+// Send Tokens
+matic.transferTokens(token, recipient, amount, {
+  from,
+  // parent: true, // For token transfer on Main network (false for Matic Network)
+  onTransactionHash: (hash) => {
+    // action on Transaction success
+    console.log(hash) // eslint-disable-line
+  },
+})
--- nodeserver/routes/routes.js
+++ nodeserver/routes/routes.js
@@ -1,6 +1,7 @@
+const bodyParser = require('body-parser');
 
 var controller = require("../controller/controller.js");
-
+var transferERC20 = require("../controller/transferERC20.js")
 var appRouter = function (app) {
 
     app.get("/", function (req, res) {
@@ -8,8 +9,20 @@ var appRouter = function (app) {
         res.status(200).send({ message: t });
   });
 
-  app.get("/hello", function(req,res){
+  app.post("/signup", function(req,res){
+      console.log(req.body.name)
       res.status(200).send({message: 'Hello Darkness my old friend'})
+  });
+  app.post("/transaction", function(req,res){
+    console.log("user exiting:" + req.body.username + " transaction worth " + req.body.cost +" ETH ")
+    res.status(200).send({message: "user exiting:" + req.body.username + " transaction worth " + req.body.cost +" ETH "})
+    
+  });
+  app.post("/checkBalance", function(req,res){
+    console.log("user exiting:" + req.body.username + " transection worth " + req.body.cost +" ETH ")
+    transferERC20.balance()
+    res.status(200).send({message: "balance"})
+    
   });
 
 }
