1f5cd56a277347b8b2078fa2df6a6ba9821c568f
Tunap <tunappaul@gmail.com>
2019-08-24 07:59:59 +0530

    address updates

--- matic/config.js
+++ matic/config.js
@@ -9,8 +9,8 @@ module.exports = {
     WATCHER_URL: 'https://ropsten-watcher2.api.matic.network/api/v1', // Backend service which syncs the Matic Plasma contract events on Ethereum mainchain to a MySQL database which we use for faster querying. This comes in handy especially for listening to asset deposits on the Plasma contract. 
     ROOTWETH_ADDRESS: '0x421dc9053cb4b51a7ec07b60c2bbb3ec3cfe050b',  // This is a wrapped ETH ERC20 contract address so that we can support ETH deposits to the sidechain 
     MATICWETH_ADDRESS: '0x31074c34a757a4b9FC45169C58068F43B717b2D0', // The corresponding wrapped ETH ERC20 contract address on the Matic chain 
-    PRIVATE_KEY: '0xFBD98D3DB498F1E4CE1CD00FC7D84381AF48B4C4C18B5D2DF04E0788D766F7D3', // A sample private key prefix with `0x`
-    FROM_ADDRESS: '0x7Cdbd798f4f8CFEe6608776CFa9B5e7B0FBb1A7B',// Your address 
+    PRIVATE_KEY: '0x9D5A3E0C3A170408B79A7ADB426AF42F8956909E51BD5C70172EFF240B70AAF1', // A sample private key prefix with `0x`
+    FROM_ADDRESS: '0x8aCdAed8CdB1d9E4c298cBDA2352C4404D49Db4f',// Your address 
     ROPSTEN_TEST_TOKEN: '0x70459e550254b9d3520a56ee95b78ee4f2dbd846', // Contract for ERC20 in Ropsten
     MATIC_TEST_TOKEN: '0xc82c13004c06E4c627cF2518612A55CE7a3Db699', // Contract for ERC20 in Matic testnet
     ROPSTEN_ERC721_TOKEN: '0x07d799252cf13c01f602779b4dce24f4e5b08bbd', // Contract for ERC721 in Ropsten testnet
