292ddeebd651146e8678e0621e32532580ee0134
Anshuman Agarwal <dev.anshuman73@gmail.com>
2019-08-24 09:36:56 +0530

    fucks

--- face-rec/views.py
+++ face-rec/views.py
@@ -67,7 +67,7 @@ def detect_person():
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
             cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
             font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, 'Hi! person.username', (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            cv2.putText(frame, 'Hi!' + person.username, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' +  cv2.imencode('.jpg', frame)[1].tostring() + b'\r\n')
 
